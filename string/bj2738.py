5850aeb0c97a203ef5c825a1f71ddbc0264ae839
한소진 <jjaren746@naver.com>
2023-01-31 18:09:41 +0900

    2738

---
+++ string/bj2738.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+n,m=map(int,input().split())
+
+a,b=[],[]
+for i in [a,b]:
+    for j in range(n):
+        i.append(list(map(int,input().split())))
+
+for i in range(n):
+    for j in range(m):
+        a[i][j]+=b[i][j]
+       # print(a[i][j], end=" ")
+    # print()
+    print(*a[i])
