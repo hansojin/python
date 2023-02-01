53e96b1a99469fadb943d2dbfca69d83431b3b76
한소진 <jjaren746@naver.com>
2023-02-02 00:34:08 +0900

    recur

---
+++ recursion/bj10870.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+n=int(input())
+def fibo(num):
+    if num==0:
+        return 0
+    elif num==1:
+        return 1
+    else:
+        return fibo(num-1)+fibo(num-2)
+print(fibo(n))
+
---
+++ recursion/bj10872.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+n=int(input())
+def fac(num):
+    if num==0 or num==1:
+        return 1
+    else:
+        return num*fac(num-1)
+print(fac(n))
