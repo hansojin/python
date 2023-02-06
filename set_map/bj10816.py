482f40b2994642488ec1d2e9dc446420c137195d
한소진 <jjaren746@naver.com>
2023-02-06 15:05:20 +0900

    counter

--- set_map/bj10816.py
+++ set_map/bj10816.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 
-import sys
-input = sys.stdin.readline
 
-n=int(input())
-n_li=list(map(int,input().split()))
-sset=set(n_li)
-m=int(input())
-m_li=list(map(int,input().split()))
+import sys
+from collections import Counter
+input=sys.stdin.readline
+
+n = int(input())
+n_li = list(map(int,input().split()))
+
+m = int(input())
+m_li = list(map(int,input().split()))
+
+c=Counter(n_li)
 
 for i in m_li:
-    if i in sset:
-        print(n_li.count(i), end=' ')
-    else: print(0, end=' ')
-
+    if i in c:
+        print(c[i], end=' ')
+    else:
+        print(0, end=' ')
