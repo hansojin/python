dbd27d6c29d48bcae8a81c716d7dc94101d6387a
한소진 <jjaren746@naver.com>
2023-02-12 12:29:33 +0900

    math

--- mathematics/bj2004.py
+++ mathematics/bj2004.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
-from math import factorial as fac
-a,b=map(int,input().split())
 
-f=fac(a)//(fac(b)*fac(a-b))
-f=str(f)[::-1]
-cnt=0
-if f[0]=='0':
-    while f[cnt]=='0':
-        cnt+=1
-print(cnt)
+m,n=map(int,input().split())
+
+def cnt_num(n,k):
+    cnt=0
+    while n:
+        n//=k
+        cnt+=n
+    return cnt
+
+five = cnt_num(m,5)-cnt_num(n,5)-cnt_num(m-n,5)
+two = cnt_num(m,2)-cnt_num(n,2)-cnt_num(m-n,2)
+
+print(min(five,two))
