06106a25141c1cae7a7fcca8ae71ce53da58ee8f
한소진 <jjaren746@naver.com>
2023-02-15 18:39:35 +0900

    dfs

---
+++ backtracking/bj15650.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+n,m=map(int,input().split())
+arr=[]
+
+def dfs(num):
+    if len(arr)==m:
+        print(*arr)
+        return
+
+    for i in range(num,n+1):
+        if i not in arr:
+            arr.append(i)
+            dfs(i+1)
+            arr.pop()
+dfs(1)
