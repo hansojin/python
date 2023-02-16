54d294bdf7b91178f7440dcd6b39ea1335e46978
한소진 <jjaren746@naver.com>
2023-02-16 21:35:02 +0900

    rm

--- greedy/bj2309.py
+++ greedy/bj2309.py
@@ -5,20 +5,15 @@ ans=0
 for _ in range(9):
     hght=int(input())
     li.append(hght)
-    ans+=hght
-
-ans-=100
-non=[]
-for i in range(0,8):
-    for j in range(i+1,9):
-            
-            if li[i]+li[j]==ans:
-                non.append(i)
-                non.append(j)
-                break
-
-del(li[non[0]])
-del(li[non[1]-1])
+    
 li.sort()
-for i in range(7):
-    print(li[i])
+for i in li:
+    for j in li:
+            
+            if sum(li) - i - j ==100:
+                li.remove(i)
+                li.remove(j)
+
+
+for i in li:
+    print(i)
