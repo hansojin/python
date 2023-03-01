bd2b61cfa462bb62accd31f74e45f13912398512
한소진 <jjaren746@naver.com>
2023-03-01 23:51:58 +0900

    int_dp

--- dynamicProgramming/bj1932.py
+++ dynamicProgramming/bj1932.py
@@ -4,8 +4,14 @@ n=int(input())
 li=[[[0] for _ in range(n)] for _ in range(n)]
 for i in range(n):
     li[i]=list(map(int,input().split()))
-j=0
-for i in range(1,n):
 
-    li[i][j]=max(li[i][j]+li[i-1][j],li[i][j]+li[i-1][j-1])
-print(max(li))
+for i in range(1,n):
+    for j in range(len(li[i])):
+        if j==0:
+            li[i][j]=li[i][j]+li[i-1][j]
+        elif j==len(li[i])-1:
+            li[i][j]=li[i][j]+li[i-1][j-1]
+        else:
+            li[i][j]=li[i][j]+max(li[i-1][j-1],li[i-1][j])
+    
+print(max(li[n-1]))
