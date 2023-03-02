37eb409036ce2f6c6f715512954b770807aa37be
한소진 <jjaren746@naver.com>
2023-03-02 16:21:11 +0900

    stair

--- dynamicProgramming/bj2579.py
+++ dynamicProgramming/bj2579.py
@@ -1,25 +1,16 @@
 #!/usr/bin/env python
 
 n=int(input())
-dp=[]*n
-li=[]*n
-for _ in range(n):
-    li.append(int(input()))
+dp=[0]*n
+li=[int(input()) for _ in range(n)]
 
-dp[0]=li[0]
-dp[1]=li[0]+li[1]
-dp[2]=li[0]+li[2]
+if len(li)<=2:
+    print(sum(li))
 
-if n==0:
-    print(dp[0])
-    exit()
-elif n==1:
-    print(dp[1])
-    exit()
-elif n==2:
-    print(dp[2])
-    exit()
+else:
+    dp[0]=li[0]
+    dp[1]=li[0]+li[1]
 
-for i in range(3,n-1):
-    dp[i]=max(dp[i-3]+li[i-1]+li[i],dp[i-2]+li[i])
-print(dp[-1])
+    for i in range(2,n):
+        dp[i]=max(dp[i-3]+li[i-1]+li[i],dp[i-2]+li[i])
+    print(dp[-1])
