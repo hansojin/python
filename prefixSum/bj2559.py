d40ed13c8a3d549a727a7353fe7e53db1397a0d1
한소진 <jjaren746@naver.com>
2023-03-09 12:22:06 +0900

    2ptr

--- prefixSum/bj2559.py
+++ prefixSum/bj2559.py
@@ -6,8 +6,10 @@ input = sys.stdin.readline
 a,b=map(int,input().split())
 li=list(map(int,input().split()))
 lis=[]
-for i in range(a-b+1):
-    lis.append(sum(li[i:i+b]))
+lis.append(sum(li[:b])) # sliding window 처럼 일단 첫 slide는 만들어주기
+
+for i in range(a-b):    # 앞ptr은 빼주고 뒷 ptr는 더해주기
+    lis.append(lis[i]-li[i]+li[i+b])
 
 
 print(max(lis))
