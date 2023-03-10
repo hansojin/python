f8fcb8cc78141a6b7fa3f31b73ed91af796495eb
한소진 <jjaren746@naver.com>
2023-03-10 15:53:41 +0900

    nested-for

--- prefixSum/bj10986.py
+++ prefixSum/bj10986.py
@@ -10,8 +10,12 @@ tmp=0
 for i in li:
     tmp+=i
     a.append(tmp)
-
-
+cnt=0
+for i in range(n+1):
+    for j in range(i+1,n+1):
+        if (a[j]-a[i])%m==0:
+            cnt+=1
+print(cnt)
 
 
 
