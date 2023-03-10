a4b9efb37ff69df54397940a28d37f0cf3570538
한소진 <jjaren746@naver.com>
2023-03-10 17:54:04 +0900

    10986 2ptr

--- prefixSum/bj10986.py
+++ prefixSum/bj10986.py
@@ -4,17 +4,27 @@ import sys
 input = sys.stdin.readline
 
 n,m=map(int,input().split())
-li=list(map(int,input().split()))
-a=[0]
-tmp=0
-for i in li:
-    tmp+=i
-    a.append(tmp)
+a=list(map(int,input().split()))
+
 cnt=0
-for i in range(n+1):
-    for j in range(i+1,n+1):
-        if (a[j]-a[i])%m==0:
+s=e=0
+subsum=a[s]
+
+while True:
+    if e<n-1:
+        e+=1
+        subsum+=a[e]
+        if subsum%m==0:
             cnt+=1
+    else:
+        s+=1
+        if s==n-1:
+            break
+        subsum=a[s]
+        if subsum%m==0:
+            cnt+=1
+        e=s
+
 print(cnt)
 
 
