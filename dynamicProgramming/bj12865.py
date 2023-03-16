11f5291db05962e7a825bab0751b429f3afb70ec
hansojin <jjaren746@naver.com>
2023-03-16 12:13:27 +0900

    knapsack

---
+++ dynamicProgramming/bj12865.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+input = sys.stdin.readline
+
+n,k=map(int,input().split())
+thing=[[0,0]]
+dp=[[0 for _ in range(k+1)] for _ in range(n+1)]
+
+for i in range(n):
+    thing.append(list(map(int,input().split())))
+
+for i in range(1,n+1):
+    for j in range(1,k+1):
+        w=thing[i][0]
+        v=thing[i][1]
+
+        if j<w:
+            dp[i][j]=dp[i-1][j]
+        else:
+            dp[i][j]=max(dp[i-1][j],dp[i-1][j-w]+v)
+#print(max(dp[-1]))
+print(dp[n][k])
