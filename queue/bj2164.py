e21b1bd148a162c397eb2f00604e3007f864d91a
hansojin <jjaren746@naver.com>
2023-03-21 15:44:03 +0900

    exception

--- queue/bj2164.py
+++ queue/bj2164.py
@@ -8,6 +8,11 @@ for i in range(1,n+1):
     q[i-1]=i
 ptr=0
 while True:
+    # in case n=1, index error
+    if cnt==0:
+        print(q[ptr])
+        break
+
     ptr+=1
     q[ptr+cnt]=q[ptr]
     cnt-=1
