04889005742f623b25eb6bf55bbb2a21ea5fe570
hansojin <jjaren746@naver.com>
2023-03-25 13:32:41 +0900

    min heap

---
+++ queue/bj1927.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import heapq
+import sys
+input = sys.stdin.readline
+
+hp=[]
+
+n=int(input())
+for i in range(n):
+    m=int(input())
+    if m==0:
+        if hp:
+            print(heapq.heappop(hp))
+        else:
+            print(0)
+    else:
+        heapq.heappush(hp,m)
+
