243a132463742854ce3941c3482d7921b901d760
hansojin <jjaren746@naver.com>
2023-04-11 13:59:25 +0900

    bfs

---
+++ graph/bj24445.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+n,m,r = map(int,input().split())
+graph= [[] for _ in range(n+1)]
+for _ in range(m):
+    a,b=map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visit=[0]*(n+1)
+cnt=1
+
+
+def bfs(srt):
+    global cnt
+    
+    q=deque([r])
+    visit[srt]=1
+    
+    while q:
+        srt=q.popleft()
+        graph[srt].sort(reverse=True)
+        for adj_node in graph[srt]:
+            if visit[adj_node]==0:
+                cnt+=1
+                visit[adj_node]=cnt
+                q.append(adj_node)
+
+bfs(r)
+
+for i in visit[1:]:
+    print(i)
