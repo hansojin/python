7cfa8957a6cfd2e6f6170b5d4b491f7da0e8c18c
hansojin <jjaren746@naver.com>
2023-05-14 11:19:49 +0900

    comb_with_replacement

--- string/bj15824.py
+++ string/bj15824.py
@@ -2,20 +2,17 @@
 
 import sys
 input= sys.stdin.readline
-from itertools import combinations
+from itertools import combinations_with_replacement
 
+ans=0
 n=int(input())
 li=list(map(int,input().split()))
-tmp=[]
+
 if n==1:
     print(li[0])
 else:
     for i in range(2,n+1):
-        sc=combinations(li,i)
-        tmp.extend(sc)
-ans=0
-for i in tmp:
-    ans+=max(i)-min(i)
+        for c in combinations_with_replacement(li,i):
+            ans+=max(c)-min(c)
+
 print(ans%1000000007)
-
-
