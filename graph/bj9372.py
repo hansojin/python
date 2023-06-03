d16ae456a7d1044f4442de5b60f2991c641f16df
hansojin <jjaren746@naver.com>
2023-06-03 12:06:38 +0900

    tree

---
+++ graph/bj9372.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import sys
+input =sys.stdin.readline
+
+t=int(input())
+for _ in range(t):
+    n,m=map(int,input().split())
+    tree=[[] for _ in range(n+1)]
+
+    for _ in range(m):
+        a,b=map(int,input().split())
+        tree[a].append(b)
+        tree[b].append(a)
+    
+    visit = [0]*(n+1)
+
+    def dfs(idx,cnt):
+        visit[idx]=1
+        for i in tree[idx]:
+            if visit[i]==0:
+                cnt=dfs(i,cnt+1)
+        return cnt
+    res=dfs(1,0)
+    print(res)
