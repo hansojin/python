b28fd1f134acb97b4b6aad2a65cb0e63e463eaf8
hansojin <jjaren746@naver.com>
2023-06-05 11:02:25 +0900

    8_10_2

---
+++ string/bj1212.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+n=str(input())
+n=n[::-1]
+num=0
+for i in range(len(n)):
+    num+=(8**i)*int(n[i])
+li=[]
+while True:
+    li.append(num%2)
+    num=num//2
+    if num==1:
+        li.append(1)
+        break
+for i in li[::-1]:
+    print(i,end='')
