7661edf5c9af272186d2fe1f55d602067a438017
hansojin <jjaren746@naver.com>
2023-06-14 10:30:52 +0900

    bf

---
+++ bruteforcing/bj10448.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+input= sys.stdin.readline
+
+def find(n):
+    for i in range(len(t)):
+        for j in range(i,len(t)):
+            for k in range(j,len(t)):
+                if t[i]+t[j]+t[k]==n:
+                    return 1
+    return 0
+
+t=[1]
+for i in range(2,45):
+    t.append(t[-1]+i)
+
+for _ in range(int(input())):
+    n=int(input())
+    print(find(n))
