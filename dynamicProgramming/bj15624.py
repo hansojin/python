481aa156ac478b119ebb032c2a1a323fb1bb236e
hansojin <jjaren746@naver.com>
2023-07-17 17:48:12 +0900

    imp

---
+++ dynamicProgramming/bj15624.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import sys
+input= sys.stdin.readline
+
+n = int(input())
+dp = [0]*(n+1)
+
+if n == 0:
+    print(0)
+else:
+    dp[0] = 0
+    dp[1] = 1
+    for i in range(2, n+1):
+        dp[i] = (dp[i-1]+dp[i-2]) % 1000000007
+    
+    print(dp[-1])
+
