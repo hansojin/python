26a6a20da3fbc9b726665195f2f8f005dc049b50
hansojin <jjaren746@naver.com>
2023-07-20 12:21:00 +0900

    kmp

--- string/bj1786.py
+++ string/bj1786.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python
 
-import sys
-input= sys.stdin.readline
+def kmp(text,pattern):
+    n=len(text)
+    m=len(pattern)
 
-t=str(input()).rstrip()
-p=str(input()).rstrip()
-pLen=len(p)-1
-t=t.replace(p,'*')
-arr,ans=[],[]
-for i in range(len(t)):
-    if t[i]=='*':
-        arr.append(i)
-print(len(arr))
-cnt=0
-for i in arr:
-    ans.append(i+cnt*pLen+1)
-    cnt+=1
-print(*ans)
+    table=[0]*m
+    
+    j=0
+    for i in range(1,m):
+        while j>0 and pattern[i]!=pattern[j]:
+            j=table[j-1]
+        if pattern[i]==pattern[j]:
+            j+=1
+            table[i]=j
+    
+    j,cnt=0,0
+    arr=[]
+    for i in range(n):
+        while j>0 and text[i]!=pattern[j]:
+            j=table[j-1]
+        if text[i]==pattern[j]:
+            if j==(m-1):
+                cnt+=1
+                arr.append(i-m+2)
+                j=table[j]
+            else:
+                j+=1
+    print(cnt)
+    print(*arr)
+
+t=input()
+p=input()
+kmp(t,p)
