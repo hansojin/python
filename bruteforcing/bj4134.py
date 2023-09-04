8b2851d26b112a653615b77e8aa506c5ca54d66a
hansojin <jjaren746@naver.com>
2023-09-04 10:17:01 +0900

    prime

---
+++ bruteforcing/bj4134.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+input= sys.stdin.readline
+import math
+
+def prime(x):
+    if x == 0 or x == 1:
+        return False
+    for i in range(2, int(math.sqrt(n))+1):
+        if x % i == 0:
+            return False
+    return True
+
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    while True:
+        if prime(n):
+            print(n)
+            break
+        else:
+            n += 1
