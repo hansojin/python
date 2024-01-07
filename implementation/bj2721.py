91b6a1972599abfec2d8c7869f5d0d926d5e8c1c
hansojin <jjaren746@naver.com>
2024-01-07 18:22:22 +0900

    tri

---
+++ implementation/bj2721.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import sys
+input= sys.stdin.readline
+
+for _ in range(int(input())):
+    n=int(input())
+    res=sum(k*sum(range(k+2)) for k in range(1,n+1))
+    print(res)
+
