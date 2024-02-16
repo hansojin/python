2e9b3ebc9a51906da9bffda2ecbef3a95bd1051b
hansojin <jjaren746@naver.com>
2024-02-16 23:52:26 +0900

    hammingDistnace

---
+++ string/bj3449.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+input= sys.stdin.readline
+
+for _ in range(int(input())):
+    a=input().rstrip()
+    b=input().rstrip()
+    cnt=0
+    for i in range(len(a)):
+        if a[i]!=b[i]:
+            cnt+=1
+    print(f'Hamming distance is {cnt}.')
