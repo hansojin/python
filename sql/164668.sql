6b35e21dfafe4e936eb13a5bb1128af1597e602c
hansojin <jjaren746@naver.com>
2024-03-22 19:10:56 +0900

    format

---
+++ sql/164668.sql
@@ -0,0 +1,8 @@
+SELECT b.USER_ID, b.NICKNAME, sum(a.PRICE) as TOTAL_SALES
+from USED_GOODS_BOARD a
+join USED_GOODS_USER b
+on a.WRITER_ID= b.USER_ID
+where a.STATUS = 'DONE'
+group by b.USER_ID
+having TOTAL_SALES>=700000
+order by TOTAL_SALES
---
+++ sql/298515.sql
@@ -0,0 +1,2 @@
+select concat(FORMAT(MAX(LENGTH),2),'cm') as MAX_LENGTH
+from FISH_INFO 
